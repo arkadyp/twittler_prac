4445bc2b39918a2cb4577a4a47264f4931ac09f3
arkadyp <arkady.pevzner@gmail.com>
2014-01-05 12:58:36 -0800

    Adds function to process hashtags by creating their own spans

--- application.js
+++ application.js
@@ -20,8 +20,29 @@ var Application = {
       $tweet.appendTo($('.tweets'));
       index -= 1;
     }
-  }
+  },
 
+  processHashtags: function(message) {
+    var words = message.split(' '); //split message into individual words
+    var spans = []; //store spans of either content or hashtags
+    var content = ""; //store non hashtag content before dumping in 'spans' 
+    //var hashtag = ""; //store hashtag before dumping into span
+    _.each(words, function(word) {
+      if(word.charAt(0) === '#') { //if word starts with hashtag, create span for it    
+        if(content !== "") { 
+          spans.push($('<span>'+content+'</span>')); //avoid creating empty content span
+        }
+        spans.push($('<span class="hashtag">'+word+'</span>'));
+        content = " ";        
+      } else {
+        content += word+" ";
+      }
+    });
+    if(content.length > 1) {  //since content span is triggered by hashtag, make sure that trailing content is added
+      spans.push($('<span>'+content+'</span>'));
+    }
+    
+  }
 };
 
 Application.init();
