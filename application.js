56073644efaa1221cee24d0093091497e5e6e7b2
arkadyp <arkady.pevzner@gmail.com>
2014-01-05 11:58:26 -0800

    Creates updateTweets function to display tweet feed

--- application.js
+++ application.js
@@ -1,18 +1,22 @@
 var Application = {
   init: function() {
-    $(document).ready(function(){
-      var $body = $('body');
-
-      var index = streams.home.length - 1;
-      while(index >= 0){
-        var tweet = streams.home[index];
-        var $tweet = $('<li></li>');
-        $tweet.text('@' + tweet.user + ': ' + tweet.message);
-        $tweet.appendTo($('.tweets'));
-        index -= 1;
-      }
+    var app = this;
+    $(document).ready(function(){      
+      app.updateTweets();          
     });
+  },
+
+  updateTweets: function() {
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = $('<li></li>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($('.tweets'));
+      index -= 1;
+    }
   }
+
 };
 
 Application.init();
